302ac7c471f13a79a6a266e37846df727d0d4e30
guoqi12345 <guoqg@student.unimelb.edu.au>
2022-10-10 17:39:56 +0800

    src

---
+++ frontend/gq src/router/router.vue
@@ -0,0 +1,33 @@
+import VueRouter from 'vue-router'
+//引入组件
+import PageP from '../pages/PageP'
+import SignIn from '../pages/SignIn'
+import SearchPage from '../pages/SearchPage'
+import ResultPage from '../pages/ResultPage'
+
+//创建并暴露一个路由器
+export default new VueRouter({
+	routes:[
+		{
+			path:'/Pages',
+			component:PageP
+		},
+		{
+			path:'/SignIn',
+			component:SignIn
+		},
+		{
+			path:'/SearchPage',
+			component:SearchPage
+		},
+		{
+			path:'/ResultPage',
+			component:ResultPage,
+			children:[
+				{
+					path:'ResultPage',
+					component:ResultPage,
+				}]
+		}
+	]
+})
