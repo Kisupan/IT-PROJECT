a1469cd741cf4b030430ffb88becd955451ce27c
congcong919 <1349030761@qq.com>
2022-10-14 21:38:42 +1100

    modify like and dislike api

--- backend3/src/Routes/Video.routes.js
+++ backend3/src/Routes/Video.routes.js
@@ -1,55 +1,55 @@
-import {response, Router } from 'express';
+import { response, Router } from 'express';
 import { Authenticate } from '../Middlewares/Authenticate.middleware';
-import {videoUpload} from '../Middlewares/Video.middleware';
-import {json} from 'body-parser'
-import{ VideoController } from '../Controller/Video.controller'
+import { videoUpload } from '../Middlewares/Video.middleware';
+import { json } from 'body-parser'
+import { VideoController } from '../Controller/Video.controller'
 
 const router = Router();
 const Controller = new VideoController();
 //after upload work, next step is save to mongo db
-router.post('/api/video', json(), Authenticate, videoUpload.single('video'), (request, response)=>{
-    Controller.upload(request,response)
+router.post('/api/video', json(), Authenticate, videoUpload.single('video'), (request, response) => {
+    Controller.upload(request, response)
 })
 
 // video stream
-router.get('/api/video/:filename', (request, response)=> {
+router.get('/api/video/:filename', (request, response) => {
     Controller.stream(request, response)
 })
 
 // like a video
-router.get('/api/video-like/:id',(request, response)=>{
+router.get('/api/video-like/', (request, response) => {
     Controller.like(request, response)
 })
 
 
 // dislike a video
-router.get('/api/video-dislike/:id',(request, response)=>{
+router.get('/api/video-dislike/', (request, response) => {
     Controller.dislike(request, response)
 })
 
 // search for a video
-router.get('/api/video-search/',(request,response)=>{
-    Controller.searchVideo (request, response)
+router.get('/api/video-search/', (request, response) => {
+    Controller.searchVideo(request, response)
 })
 
 // delete a video
-router.delete('/api/video/:video_id/:video_path', (request, response)=>{
+router.delete('/api/video/:video_id/:video_path', (request, response) => {
     Controller.delete(request, response)
 })
 
 // search for video under certain user
-router.get('/api/user-video-search/',(request,response)=>{
-    Controller.searchUVideo (request, response)
+router.get('/api/user-video-search/', (request, response) => {
+    Controller.searchUVideo(request, response)
 })
 
 // search for video under certain category
-router.get('/api/cat-search/',(request,response)=>{
-    Controller.searchCat (request, response)
+router.get('/api/cat-search/', (request, response) => {
+    Controller.searchCat(request, response)
 })
 
 //find all videos and return 
-router.get('/api/findallV',(request, response)=>{
-    Controller.findAllVideo (request, response)
+router.get('/api/findallV', (request, response) => {
+    Controller.findAllVideo(request, response)
 })
 
 export default router;
