6a6c032bfa669adfed8c7df1903aaeff826cad01
Qi Guo <guoqg@student.unimelb.edu.au>
2022-10-15 17:11:53 +0800

    search function catepage linked to video play page get searched  video from backend

--- my_vue/src/components/HomePageChannel.vue
+++ my_vue/src/components/HomePageChannel.vue
@@ -1,29 +1,56 @@
 <template>
+    <div>
     <div class="homepage-channel-navigation-bar">
-        <div class="channel-container">
-            <div class="channel-items">
-                <a href="blankpage.html" class="channel-link" target="_blank">Sports</a>
-                <a href="blankpage.html" class="channel-link" target="_blank">Music</a>
-                <a href="blankpage.html" class="channel-link" target="_blank">Games</a>
-                <a href="blankpage.html" class="channel-link" target="_blank">Food</a>
-                <a href="blankpage.html" class="channel-link" target="_blank">Fashion</a>
-                <a href="blankpage.html" class="channel-link" target="_blank">Technology</a>
-                <a href="blankpage.html" class="channel-link" target="_blank">News</a>
-                <a href="blankpage.html" class="channel-link" target="_blank">Pet</a>
-                <a href="blankpage.html" class="channel-link" target="_blank">Travel</a>
+        <div class="channel-container" >
+            <div class="channel-items"> 
+                <router-link v-for="m in messageList" :key="m.id" class="channel-link" target="_blank" :to="{
+                    path:'/Pages',
+                    query:{
+                        id:m.id,
+                        title:m.title
+                    }
+                }">
+                    {{m.title}}
+                </router-link>
             </div>
         </div>
     </div>
+   
+</div>
 </template>
 
 <script>
 export default {
-    name:"HomePageChannel"
+    name: "HomePageChannel",
+    data() {
+        return {
+            messageList: [
+                { id: '001', title: 'Sports' },
+                { id: '002', title: 'Music' },
+                { id: '003', title: 'Games' },
+                { id: '004', title: 'Fashion' },
+                { id: '005', title: 'Technology' },
+                { id: '006', title: 'News' },
+                { id: '007', title: 'Pet' },
+                { id: '008', title: 'Travel'},
+                { id: '009', title: 'Others'},
+            ]
+        }
+
+    },
 }
 </script>
-
 <style>
-    .channel-items {
+
+a {
+    text-decoration: none;
+}
+
+.router-link-active {
+    text-decoration: none;
+}
+
+.channel-items {
     grid-template-columns: repeat(9, 1fr);
     padding-right: 20px;
     display: grid;
@@ -32,33 +59,33 @@ export default {
     grid-column: span 4;
     grid-gap: 10px;
     border-right: 1px solid var(--line_regular);
-    }
+}
 
-    .channel-link {
-        font-size: large;
-        height: 22px;
-        text-decoration: none;
-        line-height: 22px;
-        display: inline-block;
-        box-sizing: content-box;
-        width: 90%;
-        height: 100%;
-        border: 1px solid var(--line_light);
-        border-radius: 10px;
-        background-color: rgb(86, 121, 182);
-        color: white;
-        text-align: center;
-        font-weight: 400;
-        transition: background-color .3s, color .3s;
-    }
+.channel-link {
+    font-size: large;
+    height: 22px;
+    text-decoration: none;
+    line-height: 22px;
+    display: inline-block;
+    box-sizing: content-box;
+    width: 90%;
+    height: 100%;
+    border: 1px solid var(--line_light);
+    border-radius: 10px;
+    background-color: rgb(86, 121, 182);
+    color: white;
+    text-align: center;
+    font-weight: 400;
+    transition: background-color .3s, color .3s;
+}
 
-    .homepage-channel-navigation-bar {
-        padding-left: 30px;
-        padding-top: 40px;
-    }
+.homepage-channel-navigation-bar {
+    padding-left: 30px;
+    padding-top: 40px;
+}
 
-    a:hover {
-        background-color: #fff;
-        color: black;
-    }
+a:hover {
+    background-color: #fff;
+    color: black;
+}
 </style>
--- my_vue/src/router/index.js
+++ my_vue/src/router/index.js
@@ -6,6 +6,12 @@ import VShareProfile from '../components/VShareProfile.vue'
 import UploadPage from '../components/UploadPage.vue'
 import ManagementSystem from '../components/ManagementSystem.vue'
 import VideoPage from '../components/VideoPlay.vue'
+import PageP from '../components/PageP'
+import SearchPage from '../components/SearchPage'
+import ResultPage from '../components/ResultPage'
+import TermPage from '../components/TermPage.vue'
+import HelpPage from '../components/HelpPage.vue'
+import ResultPage2 from '../components/ResultPage2.vue'
 const router = new VueRouter({
     routes: [
         {
@@ -37,12 +43,40 @@ const router = new VueRouter({
             path: "/video",
             component: VideoPage,
             meta: { title: 'Video' }
-        }
+        },
         // {
         //     path: '/video',
         //     component: VideoPage
         // }
-
+        {
+			path:'/Pages',
+			component:PageP
+		},
+		{
+			path:'/SearchPage',
+			component:SearchPage
+		},
+        {
+			path:'/TermPage',
+			component:TermPage
+		},
+        {
+			path:'/HelpPage',
+			component:HelpPage
+		},
+        {
+			path:'/ResultPage2',
+			component:ResultPage2
+		},
+		{
+			path:'/ResultPage',
+			component:ResultPage,
+			children:[
+				{
+					path:'ResultPage',
+					component:ResultPage,
+				}]
+		}
     ]
 })
 
