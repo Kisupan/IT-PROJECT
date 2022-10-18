e0cf4ee8ce05101e2e6c5ded819cb35828803eda
Qi Guo <guoqg@student.unimelb.edu.au>
2022-10-19 03:36:28 +0800

    solve fresh problem

--- my_vue/src/components/SearchPage.vue
+++ my_vue/src/components/SearchPage.vue
@@ -1,60 +1,47 @@
 <template>
   <div id="root">
-    <h2>your search :{{ $route.query.keyWord }}</h2>
-    <el-dropdown>
+      <h2>your search :{{ $route.query.keyWord }}</h2>
+      <el-dropdown>
       <span class="el-dropdown-link">
         sort by like<i class="el-icon-arrow-down el-icon--right"></i>
       </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item
-          ><el-button type="text" @click="sortType = 2"
-            >likes up</el-button
-          ></el-dropdown-item
-        >
-        <el-dropdown-item
-          ><el-button type="text" @click="sortType = 1"
-            >likes down</el-button
-          ></el-dropdown-item
-        >
-        <el-dropdown-item
-          ><el-button type="text" @click="sortType = 0"
-            >random</el-button
-          ></el-dropdown-item
-        >
+        <el-dropdown-item><el-button type="text"  @click="sortType = 2" >likes down</el-button></el-dropdown-item>
+        <el-dropdown-item><el-button type="text"  @click="sortType = 1">likes up</el-button></el-dropdown-item>
+        <el-dropdown-item><el-button type="text"  @click="sortType = 0">random</el-button></el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
 
-    <div v-if="!persons.length">
-      <h2 style="text-align: center">NO RESULT</h2>
-    </div>
+    <div v-if ="!persons.length"><h2 style="text-align:center">NO RESULT </h2></div>
     <div v-else>
+
       <li v-for="per of filPerons" :key="per.id">
-        <ResultPage2
-          :id="per._id"
-          :title="per.name"
-          :author="per.username"
-          :likes="per.likes"
-          :dislikes="per.dislikes"
-          :label="per.category"
-          :path="per.videopath"
-          :description="per.description"
-        >
-        </ResultPage2>
-      </li>
+      <ResultPage
+        :id="per._id"
+        :title="per.name"
+        :author="per.username"
+        :likes="per.likes"
+        :dislikes="per.dislikes"
+        :label="per.category"
+        :path="per.videopath"
+        :description="per.description"
+      >
+      </ResultPage>
+    </li>
     </div>
   </div>
 </template>
 <router-view :key='$route.fullPath'></router-view >
 <script type="text/javascript"></script>
 <script>
-import ResultPage2 from "./ResultPage2.vue";
+import ResultPage from "./ResultPage.vue";
 export default {
   components: {
-    ResultPage2,
+    ResultPage,
   },
   data() {
     return {
-      sortType: 0,
+      sortType:0,
       keyword: this.$route.query.keyWord,
       persons: {},
     };
@@ -104,20 +91,25 @@ export default {
       },
     },
   },
-  computed: {
-    filPerons() {
-      //判断一下是否需要排序
-      if (this.sortType) {
-        this.persons.sort((p1, p2) => {
-          return this.sortType === 1
-            ? p2.likes - p1.likes
-            : p1.likes - p2.likes;
-        });
-      }
-      console.log(this.persons);
-      return this.persons;
+  watch: {
+    sortType: {
+      immediate: true,
+      deep: true,
+
     },
   },
+  computed:{
+					filPerons(){
+						//判断一下是否需要排序
+						if(this.sortType){
+							this.persons.sort((p1,p2)=>{
+								return this.sortType === 1 ? p2.likes-p1.likes : p1.likes-p2.likes
+							})
+				}
+        console.log(this.persons);
+        return this.persons
+					}
+      },
   name: "SearchPage",
 };
 </script>
